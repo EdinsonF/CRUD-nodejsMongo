c670a57d41d0f72272f32cc2b841773cf5e4a7d1
edinjson <figueroaedinson@gmail.com>
2020-08-25 19:28:29 -0500

    finalizado

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 
 //conectar a base de datos
@@ -22,6 +24,24 @@ app.set('view engine', 'ejs');
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+}));
+app.use(flash());
+
+//VARIABLES GLOBALES
+app.use((req, res, next) =>{
+    res.locals.mensaje = req.flash('mensaje');
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.locals.mensajeNo = req.flash('mensajeNo');
+    next();
+});
+
 //rutas
 app.use('/', indexRoutes);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,12 +11,36 @@ router.get('/', async (req, res) =>{
 router.post('/add', async (req, res) =>{
     const registros = new Task(req.body);
     await registros.save();
+    req.flash('mensaje', 'Registro exitoso');
     res.redirect('/');
 });
 
 router.get('/delete/:id', async (req, res) =>{
     const {id} = req.params;
     await Task.remove({_id: id});
+    req.flash('mensajeNo', 'Registro Eliminado');
+    res.redirect('/');
+});
+
+router.get('/status/:id', async (req, res) =>{
+    const {id} = req.params;
+    const result = await Task.findById(id);
+    result.estado = !result.estado;
+    await result.save({result});
+    req.flash('mensaje', 'Estado cambiado');
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) =>{
+    const {id} = req.params;
+    const result = await Task.findById(id);
+    res.render('edit',{result});
+});
+
+router.post('/edit/:id', async (req, res) =>{
+    const {id} = req.params;
+    await Task.update({_id : id}, req.body);
+    req.flash('mensaje', 'Registro actualizado');
     res.redirect('/');
 });
 
